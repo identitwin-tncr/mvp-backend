c1fac1f6a6ed25c10e1367fbfe8a7cb3d8d2c4a4
XoulEater <ncqueescribir@gmail.com>
2024-08-24 22:55:14 -0600

    bd diagram

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import session from 'express-session';
+import express from "express";
+import cors from "cors";
+import session from "express-session";
 
-import unprotectedRouter from './lib/routes/unprotected_routes';
-import catalogRouter from './lib/routes/catalog_routes';
-import elementRouter from './lib/routes/elements_routes';
-import instrumentRouter from './lib/routes/instruments_routes';
-import alarmRouter from './lib/routes/alarms_routes';
-import conservationActionRouter from './lib/routes/conservation_actions_routes';
-import woundRouter from './lib/routes/wound_routes'
-import {scheduleAlarms} from "./lib/controllers/alarms_controller";
+import unprotectedRouter from "./lib/routes/unprotected_routes";
+import catalogRouter from "./lib/routes/catalog_routes";
+import elementRouter from "./lib/routes/elements_routes";
+import instrumentRouter from "./lib/routes/instruments_routes";
+import alarmRouter from "./lib/routes/alarms_routes";
+import conservationActionRouter from "./lib/routes/conservation_actions_routes";
+import woundRouter from "./lib/routes/wound_routes";
+import { scheduleAlarms } from "./lib/controllers/alarms_controller";
 
 /**
  * Import environmental variables
@@ -24,7 +24,7 @@ const app = express();
 /**
  * Express configuration
  */
-app.set('port', API_PORT);
+app.set("port", API_PORT);
 app.use(cors());
 app.use(express.json());
 app.use(
@@ -32,7 +32,7 @@ app.use(
         secret: EXPRESS_SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-    }),
+    })
 );
 scheduleAlarms();
 
@@ -40,12 +40,15 @@ scheduleAlarms();
  * App routes
  */
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.use(unprotectedRouter);
-app.use('/catalog', catalogRouter); //Monitoring variables, Materials
-app.use('/elements', elementRouter); //structural, decorative
-app.use('/instruments', instrumentRouter); //Datalogger, others.
-app.use('/alarms', alarmRouter); //alarm historial
-app.use('/conservationAccion', conservationActionRouter); //Conservation actions associated to elements, material and rank
-app.use('/wound', woundRouter);
+app.use("/catalog", catalogRouter); //Monitoring variables, Materials
+app.use("/elements", elementRouter); //structural, decorative
+app.use("/instruments", instrumentRouter); //Datalogger, others.
+app.use("/alarms", alarmRouter); //alarm historial
+app.use("/conservationAccion", conservationActionRouter); //Conservation actions associated to elements, material and rank
+app.use("/wound", woundRouter);
 
 export default app;
